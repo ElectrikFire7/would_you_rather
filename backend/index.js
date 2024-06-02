0454a54b0204bb99f682bf65fb3d97c5c5c8b4fb
Yolohama <9kishlay@gmail.com>
2024-06-02 13:44:45 +0530

    backend vercel orgins fix

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-    origin: "*", 
+    origin: ["http://localhost:5173"], 
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204

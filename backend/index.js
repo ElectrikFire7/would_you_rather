3d8a7dccd495cdd03240d3eeb004dadedb4a9fe4
Yolohama <9kishlay@gmail.com>
2024-06-02 13:48:38 +0530

    index.js backend update

--- backend/index.js
+++ backend/index.js
@@ -12,8 +12,6 @@ app.use(express.json());
 const corsOptions = {
     origin: ["http://localhost:5173"], 
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204
 };
 
 app.use(cors(corsOptions));

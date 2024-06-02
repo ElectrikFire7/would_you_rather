e918833b136867ee29ca8561a186d2dae2c1795e
Yolohama <9kishlay@gmail.com>
2024-06-02 14:08:42 +0530

    cors open to all for bulk upload

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,11 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-    origin: ["http://localhost:5173", "https://would-you-rather-blue.vercel.app/"], 
+    origin: ["http://localhost:5173", "https://would-you-rather-1tns.onrender.com/"], 
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use("/user", userRoute);
 app.use("/question", questionRoute);
